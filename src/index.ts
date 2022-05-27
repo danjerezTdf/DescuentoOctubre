2b6bec531fd6afbe33939ebe3bccad36437614b6
danjerezTdf <104039810+danjerezTdf@users.noreply.github.com>
2022-05-27 15:16:31 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
+/* Ejercicio – Descuento Octubre
+• Una tienda al cumplir años en Octubre ofrece
+un descuento del 15% a sus clientes en todas
+sus compras
+• Desarrolle un algoritmo que dada una compra:
+precio unitario, cantidad y el mes, indicados
+por el usuario, determine si el cliente tiene
+descuento o no */
+
 let precioUnitario: number = Number(prompt(`Ingreser Precio Unitario:`));
 let cantidad: number = Number(prompt(`Ingrese Cantidad`));
 let mesCompra: number = Number(
